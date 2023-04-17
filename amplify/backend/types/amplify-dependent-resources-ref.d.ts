3080216bdfd8696425a3cc67f38d61997ef12bb2
Paul Brennan <paul.brennan@xdesign.com>
2023-04-17 10:58:41 +0100

    add graphql

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "letsbook": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "letsbook7d4bd043": {
       "AppClientID": "string",
